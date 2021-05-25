a22c3350fb86330dfd95880b313c452e68b2f95e
NegiBaba <piyushnegi319@gmail.com>
2021-05-26 01:31:10 +0530

    made some changes and added some jquery

---
+++ resources/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".about-section").waypoint(function(direction) {
+		if(direction == "down") {
+			$("nav").addClass("sticky-nav");
+		} else {
+			$("nav").removeClass("sticky-nav");
+		}
+	});
+});
